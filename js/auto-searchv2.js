b750061094e9cb4b179206ee8c25847f7f7bbaa5
Parveen Bhadoo <87309724+PBhadoo@users.noreply.github.com>
2023-05-12 09:32:38 +0530

    Update auto-searchv2.js

--- js/auto-searchv2.js
+++ js/auto-searchv2.js
@@ -1,1 +1,31 @@
-//moved to <script src="https://1337x.cdn.zindex.eu.org/auto-searchv2.js"></script>
+// JavaScript Document
+  $(function() {
+    var cache = {};
+    $( "#autocomplete" ).autocomplete({
+      minLength: 3,
+      appendTo: '.search-box',
+      source: function( request, response ) {
+        var term = request.term;
+        if ( term in cache ) {
+          response( cache[ term ] );
+          return;
+        }
+        $.getJSON( "//"+server+"/cdnsuggest.php", request, function( data, status, xhr ) {
+          cache[ term ] = data.data;
+          response( data.data );
+        });
+      },
+      select: function(event, ui) { 
+       //$("#search-form").submit();
+      }
+    }).data('uiAutocomplete')._renderItem = function( ul, item ) {
+      var srchTerm = this.term.trim().split(/\s+/).join ('|');
+      var strNewLabel = item.label;
+      regexp = new RegExp ('(' + srchTerm + ')', "ig");
+      var strNewLabel = strNewLabel.replace(regexp,"<span class='highlight-class'>$1</span>");
+      return $( "<li></li>" )
+      .data( "item.autocomplete", item )
+      .append( "<a>" + strNewLabel + "</a>" )
+      .appendTo( ul );
+   };
+  });
