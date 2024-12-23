c1c99cacc43d41f2509794db9a68d12b030236e6
Parveen Bhadoo <87309724+PBhadoo@users.noreply.github.com>
2024-12-23 09:31:20 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,127 @@
+var newtime = Date.now()
+// check page path
+var path = window.location.pathname;
+var html = `<div class="mobile-menu"></div>
+<div class="top-bar">
+<div class="container">
+<div class="top-bar-left">
+</div>
+<ul class="top-bar-nav">
+</ul>
+</div>
+</div>
+<header>
+<div class="container">
+<a href="#" class="navbar-menu"><span></span><span></span><span></span></a>
+<nav>
+<ul class="main-navigation">
+<li class="green"><a href="/processor/?path=/`+newtime+`/home/" title="Full Home Page">Full Home Page</a></li>
+<li><a href="/processor/?path=/`+newtime+`/top-100" title="Top 100 Torrents">Top 100</a></li>
+<li><a href="/processor/?path=/`+newtime+`/trending" title="Trending Torrents">Trending</a></li>
+<li><a href="https://telegram.dog/joinchat/5PYMXpqIV2M4MDJh" title="Telegram">TG Group</a></li> 
+<li><a href="https://telegram.dog/HashHackers" title="Telegram">TG Channel</a></li> 
+</ul>
+</nav>
+</div>
+</header>
+<main class="container">
+<div class="row">
+<div class="col-8 col-push-2 page-content ">
+<div class="logo"><a href="https://hashhackers.com" style="color:white;"><img alt="logo" src="https://cdn.jsdelivr.net/gh/1337-to/homepage-cdn@2.0/images/logo.svg"><img alt="logo" src="https://cdn.jsdelivr.net/gh/1337-to/homepage-cdn@2.0/images/logo-white-d.svg"></a>High Speed 1337x Proxy Network</div>
+<div class="search-box">
+<form id="search-index-form" method="get" action="">
+  <input type="search" placeholder="Search for torrents.." id="autocomplete" name="search" class="ui-autocomplete-input form-control">
+  <button type="submit" class="btn btn-search"><i class="flaticon-search"></i><span>Search</span></button>
+</form>
+</div>
+<div class="news">
+<div class="box-info">
+<div class="box-info-heading clearfix"><h1>Avoid Fake Websites</h1> <span class="box-info-right box-info-time"><i class="flaticon-time"></i>Today</span></div>
+<div class="box-info-detail clearfix">
+<p>This is a CDN-based proxy service for the official 1337x website. Please note that 1337x.to is the original website. Visiting any other site may expose you to fake websites and potential risks.</p>
+<hr>
+<p>This proxy is managed by Hash Hackers and can be verified by comparing the data and magnet links on both the original and proxy websites. For added convenience, you can click on the logo above to access the homepage, which lists other proxy websites.</p>
+<p>Your safety is our priority:</p>
+<p></p>
+<p>1. To protect your credentials, logging in through this proxy service is not permitted.</p>
+<p>2. The main domain (hashhackers.com) does not host any copyrighted materials. Proxy domains only facilitate access to data and do not directly host any content.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</main>
+<ul class="search-categories">
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/popular-movies" title="Movies"><span class="icon"><i class="flaticon-movies"></i></span>Movies </a></h3>
+<a href="/processor/?path=/`+newtime+`/movie-library/1/" class="library">Library</a>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/popular-tv" title="Television"><span class="icon"><i class="flaticon-tv"></i></span>Television </a></h3>
+<a href="/processor/?path=/`+newtime+`/series-library/a/1/" class="library">Library</a>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/popular-games" title="Games"><span class="icon"><i class="flaticon-games"></i></span>Games </a></h3>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/popular-music" title="Musics"><span class="icon"><i class="flaticon-music"></i></span>Music </a></h3>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/popular-apps" title="Applications"><span class="icon"><i class="flaticon-apps"></i></span>Applications </a></h3>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/cat/Anime/1/" title="Other"><span class="icon"><i class="flaticon-ninja-portrait"></i></span>Anime </a></h3>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/cat/Documentaries/1/" title="Documentries"><span class="icon"><i class="flaticon-documentary"></i></span>Documentaries </a></h3>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/popular-other" title="Other"><span class="icon"><i class="flaticon-other"></i></span>Other </a></h3>
+</li>
+<li>
+<h3><a href="/processor/?path=/`+newtime+`/popular-xxx" title="XXX"><span class="icon"><i class="flaticon-xxx"></i></span>XXX </a></h3>
+</li>
+</ul>
+<footer>
+<div class="bitcoin">
+<div class="bitcoin-icon-wrap">
+<span class="bitcoin-icon"><i class="flaticon-bitcoin red"></i></span>
+</div>
+<span class="bitcoin-text"><span>Bitcoin Donate: </span><a href="https://www.blockchain.com/explorer/addresses/btc/1337xNFVMCUzTpRSjZDrzNx2GKEiGDbVsS">1337xNFVMCUzTpRSjZDrzNx2GKEiGDbVsS</a></span>
+</div>
+<a class="scroll-top" href="#"><i class="flaticon-up"></i></a>
+<ul>
+<li><a href="https://1337x.hashhackers.com">Home</a></li>
+<li class="active"><a href="/processor/?path=/`+newtime+`/home/">Full Home Page</a></li>
+<li><a href="https://telegram.dog/joinchat/5PYMXpqIV2M4MDJh">TG Group</a></li> 
+<li><a href="https://telegram.dog/HashHackers">TG Channel</a></li>
+<li><a href="https://github.com/1337-to/homepage-cdn">GitHub</a></li>
+</ul>
+<p class="info">1337x 1001 - 2050, just a Proxy!</p>
+<p class="info"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://1337x.hashhackers.com&amp;count_bg=%2379C83D&amp;title_bg=%23555555&amp;icon=&amp;icon_color=%23E7E7E7&amp;title=Daily/Total Views&amp;edge_flat=false"></p>
+<p class="info"><img src="https://data.jsdelivr.com/v1/package/gh/1337-to/homepage-cdn/badge/year"></p>
+<p class="info">This is just a proxy site, dmca original site and data from here will be automatically removed.</p>
+<p class="info">DMCA? Think about it carefully, this website or domain doesn't host any Copyrighted Material.</p>
+<p class="info">More importantly, the websites it redirects to, also are just proxies with less than 50 KB of JS code each.</p>
+<p class="info">Go to Official site 1337x.to, if you're trying to file a Copyright claim.</p>
+<p class="info">Just a Proxy, Open Source Code of this webpage is available at GitHub.</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/gh/1337-to/homepage-cdn@2.0/js/auto-searchv2.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/1337-to/homepage-cdn@2.0/js/main.js"></script>
+<script>
+  document.getElementById("search-index-form").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission
+
+    var form = event.target;
+    var searchQuery = document.getElementById('autocomplete').value;
+
+    // Set the action dynamically using string concatenation
+    form.action = '/processor/?path=/' + newtime + '/srch&search=' + encodeURIComponent(searchQuery);
+
+    // Now submit the form programmatically
+    form.submit();
+  });
+</script>
+`;
+
+$('body').html(html);
